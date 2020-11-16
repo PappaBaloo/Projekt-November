1e4c5379c7c9f82fbe7b0c7c8327e79a75406910
Charlie Sesser Nylund <charlie.sessernylun@learnet.se>
2020-11-16 09:23:29 +0100

    first commit

---
+++ base defender/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Raylib_cs;
+
+namespace base_defender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "Hello World");
+
+           while (!Raylib.WindowShouldClose())
+           {
+
+           Raylib.BeginDrawing();
+
+
+           }
+
+        }
+    }
+}
